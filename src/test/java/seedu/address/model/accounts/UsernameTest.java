2846b5a0684c960364a9407aabe76a85031ba235
AZhiKai <ang-zhikai@outlook.com>
2018-09-29 00:58:53 +0800

    UsernameTest: Add test for Username

---
+++ src/test/java/seedu/address/model/accounts/UsernameTest.java
@@ -0,0 +1,43 @@
+package seedu.address.model.accounts;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.model.person.Name;
+import seedu.address.testutil.Assert;
+
+public class UsernameTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Username(null));
+    }
+
+    @Test
+    public void constructor_invalidName_throwsIllegalArgumentException() {
+        String invalidUsername = "";
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Username(invalidUsername));
+    }
+
+    @Test
+    public void isValidUsername() {
+        // null username
+        Assert.assertThrows(NullPointerException.class, () -> Username.isValidUsername(null));
+
+        // invalid username
+        assertFalse(Username.isValidUsername("")); // empty string
+        assertFalse(Username.isValidUsername(" ")); // spaces only
+        assertFalse(Username.isValidUsername("^")); // only non-alphanumeric characters
+        assertFalse(Username.isValidUsername("peter*")); // contains non-alphanumeric characters
+        assertFalse(Username.isValidUsername("azhi kai")); // contains space
+
+        // valid username
+        assertTrue(Username.isValidUsername("azhikai")); // alphabets only
+        assertTrue(Username.isValidUsername("12345")); // numbers only
+        assertTrue(Username.isValidUsername("peter2")); // alphanumeric characters
+        assertTrue(Username.isValidUsername("CapitalAng")); // with capital letters
+        assertTrue(Username.isValidUsername("ThisIsALongUsernameAlthoughWhoWouldDoThisRight")); // long names
+    }
+}
