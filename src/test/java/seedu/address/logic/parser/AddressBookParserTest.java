e6b521ad7fe2b807a5418eb21616d721dae91b2c
m4dkip <ongmingxian@gmail.com>
2018-09-29 20:12:55 +0800

    Logic: AddressBookParserTest: Add test for Delete command alias

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -60,6 +60,9 @@ public class AddressBookParserTest {
         DeleteCommand command = (DeleteCommand) parser.parseCommand(
                 DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
         assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
+        command = (DeleteCommand) parser.parseCommand(DeleteCommand.COMMAND_ALIAS
+                + " " + INDEX_FIRST_PERSON.getOneBased());
+        assertEquals(new DeleteCommand(INDEX_FIRST_PERSON), command);
     }
 
     @Test
