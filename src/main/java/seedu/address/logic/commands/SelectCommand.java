131be1cfd7a03e5633b93ec9c41c4317acf0ff68
m4dkip <ongmingxian@gmail.com>
2018-09-29 15:29:47 +0800

    Logic: Add aliases for Add, Clear, Delete, Edit, Find, List, Select, Redo, Undo commands

--- src/main/java/seedu/address/logic/commands/SelectCommand.java
+++ src/main/java/seedu/address/logic/commands/SelectCommand.java
@@ -20,6 +20,8 @@ public class SelectCommand extends Command {
 
     public static final String COMMAND_WORD = "select";
 
+    public static final String COMMAND_ALIAS = "s";
+
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Selects the person identified by the index number used in the displayed person list.\n"
             + "Parameters: INDEX (must be a positive integer)\n"
