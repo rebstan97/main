185bf1255f4a09a4ba15cbbf225089f1b5030be3
m4dkip <ongmingxian@gmail.com>
2018-10-02 22:05:55 +0800

    model/reservation/exceptions: Update DuplicateReservationException

--- src/main/java/seedu/address/model/reservation/exceptions/DuplicateReservationException.java
+++ src/main/java/seedu/address/model/reservation/exceptions/DuplicateReservationException.java
@@ -1,11 +1,11 @@
-package seedu.address.model.person.exceptions;
+package seedu.address.model.reservation.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Persons (Persons are considered duplicates if they have the same
- * identity).
+ * Signals that the operation will result in duplicate Reservations
+ * (Reservations are considered duplicates if they have the same identity).
  */
-public class DuplicatePersonException extends RuntimeException {
-    public DuplicatePersonException() {
-        super("Operation would result in duplicate persons");
+public class DuplicateReservationException extends RuntimeException {
+    public DuplicateReservationException() {
+        super("Operation would result in duplicate reservations");
     }
 }
