294ac80d7f5cc1f09dccb4dba0f758660b032977
yican95 <yican95@gmail.com>
2018-10-03 15:43:59 +0800

    Add testing util for menu management

---
+++ src/test/java/seedu/address/testutil/menu/ItemBuilder.java
@@ -0,0 +1,80 @@
+package seedu.address.testutil.menu;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import seedu.address.model.menu.Item;
+import seedu.address.model.menu.Name;
+import seedu.address.model.menu.Price;
+import seedu.address.model.menu.Remark;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
+
+/**
+ * A utility class to help with building Person objects.
+ */
+public class ItemBuilder {
+
+    public static final String DEFAULT_NAME = "Cheese Fries";
+    public static final String DEFAULT_PRICE = "2";
+    public static final String DEFAULT_REMARK = "";
+
+    private Name name;
+    private Price price;
+    private Remark remark;
+    private Set<Tag> tags;
+
+    public ItemBuilder() {
+        name = new Name(DEFAULT_NAME);
+        price = new Price(DEFAULT_PRICE);
+        remark = new Remark(DEFAULT_REMARK);
+        tags = new HashSet<>();
+    }
+
+    /**
+     * Initializes the ItemBuilder with the data of {@code itemToCopy}.
+     */
+    public ItemBuilder(Item itemToCopy) {
+        name = itemToCopy.getName();
+        price = itemToCopy.getPrice();
+        remark = itemToCopy.getRemark();
+        tags = new HashSet<>(itemToCopy.getTags());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Item} that we are building.
+     */
+    public ItemBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
+     */
+    public ItemBuilder withTags(String ... tags) {
+        this.tags = SampleDataUtil.getTagSet(tags);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Price} of the {@code Item} that we are building.
+     */
+    public ItemBuilder withPrice(String price) {
+        this.price = new Price(price);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Price} of the {@code Item} that we are building.
+     */
+    public ItemBuilder withRemark(String remark) {
+        this.remark = new Remark(remark);
+        return this;
+    }
+
+    public Item build() {
+        return new Item(name, price, remark, tags);
+    }
+
+}
