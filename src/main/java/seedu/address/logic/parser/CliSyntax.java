28e728a5a13cba0fcf20a33e1ab27c94a68c8c9e
yican95 <yican95@gmail.com>
2018-10-03 15:42:45 +0800

    CliSyntax.java: Add prefix price for menu management

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -13,4 +13,7 @@ public class CliSyntax {
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_REMARK = new Prefix("r/");
 
+    // Menu Management
+    public static final Prefix PREFIX_PRICE = new Prefix("p/");
+
 }
