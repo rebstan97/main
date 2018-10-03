501c9707cedae0b2358cbfce63bdb96090ccec52
m4dkip <ongmingxian@gmail.com>
2018-10-03 22:38:28 +0800

    AddressBook.java: Integrate Reservation into addressbook for reservation management

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -9,6 +9,8 @@ import java.util.Set;
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.reservation.Reservation;
+import seedu.address.model.reservation.UniqueReservationList;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -17,6 +19,7 @@ import seedu.address.model.tag.Tag;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePersonList persons;
+    private final UniqueReservationList reservations;
 
     /*
      * The 'unusual' code block below is an non-static initialization block, sometimes used to avoid duplication
@@ -27,6 +30,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     {
         persons = new UniquePersonList();
+        reservations = new UniqueReservationList();
     }
 
     public AddressBook() {}
@@ -118,6 +122,67 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     public void removeTag(Tag tag) {
         persons.forEach(person -> removeTagForPerson(person, tag));
+    }
+
+    // Reservation Management
+
+    /**
+     * Replaces the contents of the reservation list with {@code reservations}. {@code reservations} must not contain
+     * duplicate reservations.
+     */
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations.setReservations(reservations);
+    }
+
+    /**
+     * Returns true if a reservation with the same identity as {@code reservation} exists in the address book.
+     */
+    public boolean hasReservation(Reservation reservation) {
+        requireNonNull(reservation);
+        return reservations.contains(reservation);
+    }
+
+    /**
+     * Adds a reservation to the address book. The reservation must not already exist in the address book.
+     */
+    public void addReservation(Reservation r) {
+        reservations.add(r);
+    }
+
+    /**
+     * Replaces the given reservation {@code target} in the list with {@code editedReservation}. {@code target} must
+     * exist in the address book. The person identity of {@code editedReservation} must not be the same as
+     * another existing person in the address book.
+     */
+    public void updateReservation(Reservation target, Reservation editedReservation) {
+        requireNonNull(editedReservation);
+
+        reservations.setReservation(target, editedReservation);
+    }
+
+    /**
+     * Removes {@code key} from this {@code AddressBook}. {@code key} must exist in the address book.
+     */
+    public void removeReservation(Reservation key) {
+        reservations.remove(key);
+    }
+
+    /**
+     * Removes {@code tag} from {@code reservation} in this {@code AddressBook}.
+     *
+     * @param reservation whose tag is being removed.
+     * @param tag to be removed.
+     */
+    private void removeTagForReservation(Reservation reservation, Tag tag) {
+        Set<Tag> tags = new HashSet<>(reservation.getTags());
+
+        if (!tags.remove(tag)) {
+            return;
+        }
+
+        Reservation newReservation = new Reservation(reservation.getName(), reservation.getPax(),
+                reservation.getDateTime(), reservation.getRemark(), tags);
+        updateReservation(reservation, newReservation);
     }
 
     //// util methods
