33d76a2d9b944060f5cccced5933d19f849fc396
yican95 <yican95@gmail.com>
2018-10-03 20:19:08 +0800

    Add XmlAdaptedItem.java

---
+++ src/main/java/seedu/address/storage/menu/XmlAdaptedItem.java
@@ -0,0 +1,120 @@
+package seedu.address.storage.menu;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.xml.bind.annotation.XmlElement;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.menu.Item;
+import seedu.address.model.menu.Name;
+import seedu.address.model.menu.Price;
+import seedu.address.model.menu.Remark;
+import seedu.address.model.tag.Tag;
+import seedu.address.storage.XmlAdaptedTag;
+
+/**
+ * JAXB-friendly version of the Person.
+ */
+public class XmlAdaptedItem {
+
+    public static final String MISSING_FIELD_MESSAGE_FORMAT = "Item's %s field is missing!";
+
+    @XmlElement(required = true)
+    private String name;
+    @XmlElement(required = true)
+    private String price;
+    @XmlElement(required = true)
+    private String remark;
+
+    @XmlElement
+    private List<XmlAdaptedTag> tagged = new ArrayList<>();
+
+    /**
+     * Constructs an XmlAdaptedPerson.
+     * This is the no-arg constructor that is required by JAXB.
+     */
+    public XmlAdaptedItem() {}
+
+    /**
+     * Constructs an {@code XmlAdaptedPerson} with the given person details.
+     */
+    public XmlAdaptedItem(String name, String price, String remark, List<XmlAdaptedTag> tagged) {
+        this.name = name;
+        this.price = price;
+        this.remark = remark;
+        if (tagged != null) {
+            this.tagged = new ArrayList<>(tagged);
+        }
+    }
+
+    /**
+     * Converts a given Item into this class for JAXB use.
+     *
+     * @param source future changes to this will not affect the created XmlAdaptedItem
+     */
+    public XmlAdaptedItem(Item source) {
+        name = source.getName().fullName;
+        price = source.getPrice().value;
+        remark = source.getRemark().value;
+        tagged = source.getTags().stream()
+                .map(XmlAdaptedTag::new)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Converts this jaxb-friendly adapted person object into the model's Person object.
+     *
+     * @throws IllegalValueException if there were any data constraints violated in the adapted person
+     */
+    public Item toModelType() throws IllegalValueException {
+        final List<Tag> itemTags = new ArrayList<>();
+        for (XmlAdaptedTag tag : tagged) {
+            itemTags.add(tag.toModelType());
+        }
+
+        if (name == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+        }
+        if (!Name.isValidName(name)) {
+            throw new IllegalValueException(Name.MESSAGE_NAME_CONSTRAINTS);
+        }
+        final Name modelName = new Name(name);
+
+        if (price == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Price.class.getSimpleName()));
+        }
+        if (!Price.isValidPrice(price)) {
+            throw new IllegalValueException(Price.MESSAGE_PRICE_CONSTRAINTS);
+        }
+        final Price modelPrice = new Price(price);
+
+        if (remark == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Remark.class.getSimpleName()));
+        }
+        final Remark modelRemark = new Remark(remark);
+
+        final Set<Tag> modelTags = new HashSet<>(itemTags);
+        return new Item(modelName, modelPrice, modelRemark, modelTags);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof XmlAdaptedItem)) {
+            return false;
+        }
+
+        XmlAdaptedItem otherItem = (XmlAdaptedItem) other;
+        return Objects.equals(name, otherItem.name)
+                && Objects.equals(price, otherItem.price)
+                && tagged.equals(otherItem.tagged);
+    }
+}
