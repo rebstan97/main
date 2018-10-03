7e6bf53133b5a2f9c43275190b999303de8b96b7
HyperionNKJ <kjneo1996@gmail.com>
2018-10-04 04:42:11 +0800

    CommandTestUtil: Add valid and invalid record attributes

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -3,9 +3,13 @@ package seedu.address.logic.commands;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ITEM_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ITEM_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_QUANTITY_SOLD;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.ArrayList;
@@ -56,6 +60,31 @@ public class CommandTestUtil {
     public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
     public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
     public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
+
+    /** For sales records */
+    public static final String VALID_DATE_DEMO_ONE = "28-02-2018";
+    public static final String VALID_DATE_DEMO_TWO = "11-11-2011";
+    public static final String VALID_DATE_DEMO_THREE = "25-12-2017";
+    public static final String VALID_ITEM_NAME_DEMO_ONE = "Fried Rice";
+    public static final String VALID_ITEM_NAME_DEMO_TWO = "Pasta";
+    public static final String VALID_ITEM_NAME_DEMO_THREE = "Orange Juice";
+    public static final String VALID_QUANTITY_SOLD_DEMO_ONE = "100";
+    public static final String VALID_QUANTITY_SOLD_DEMO_TWO = "87";
+    public static final String VALID_QUANTITY_SOLD_DEMO_THREE = "202";
+    public static final String VALID_PRICE_DEMO_ONE = "8.50";
+    public static final String VALID_PRICE_DEMO_TWO = "7.99";
+    public static final String VALID_PRICE_DEMO_THREE = "2";
+
+    public static final String PREFIX_WITH_VALID_DATE = " " + PREFIX_DATE + VALID_DATE_DEMO_ONE;
+    public static final String PREFIX_WITH_VALID_ITEM_NAME = " " + PREFIX_ITEM_NAME + VALID_ITEM_NAME_DEMO_ONE;
+    public static final String PREFIX_WITH_VALID_QUANTITY_SOLD = " " + PREFIX_QUANTITY_SOLD + VALID_QUANTITY_SOLD_DEMO_ONE;
+    public static final String PREFIX_WITH_VALID_PRICE = " " + PREFIX_ITEM_PRICE + VALID_PRICE_DEMO_ONE;
+    public static final String PREFIX_WITH_INVALID_DATE = " " + PREFIX_DATE + "31-02-2018"; // no such date
+    public static final String PREFIX_WITH_INVALID_ITEM_NAME = " " + PREFIX_ITEM_NAME + "Fried Rice!"; // symbols not
+    // allowed
+    public static final String PREFIX_WITH_INVALID_QUANTITY_SOLD = " " + PREFIX_QUANTITY_SOLD + "3.5"; // positive
+    // integer only
+    public static final String PREFIX_WITH_INVALID_PRICE = " " + PREFIX_ITEM_PRICE + "-2"; // negative price not allowed
 
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
