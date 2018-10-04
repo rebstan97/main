7fc2d758fb2ab561724353dade945569565f6a2f
yican95 <yican95@gmail.com>
2018-10-04 17:12:10 +0800

    AddressBookChangedEvent.java: Update to support menu management

--- src/main/java/seedu/address/commons/events/model/AddressBookChangedEvent.java
+++ src/main/java/seedu/address/commons/events/model/AddressBookChangedEvent.java
@@ -14,6 +14,7 @@ public class AddressBookChangedEvent extends BaseEvent {
 
     @Override
     public String toString() {
-        return "number of persons " + data.getPersonList().size();
+        return "number of persons " + data.getPersonList().size()
+                + ", number of items " + data.getItemList().size();
     }
 }
