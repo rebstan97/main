2f5cdfa622576f1b6164da579331441446937cc6
Rebecca Tan <rebecca_tan_97@hotmail.com>
2018-10-04 20:18:38 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/model/ingredient/exceptions/DuplicateIngredientException.java
+++ src/main/java/seedu/address/model/ingredient/exceptions/DuplicateIngredientException.java
@@ -1,8 +1,8 @@
 package seedu.address.model.ingredient.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Ingredients (Ingredients are considered duplicates if they have
- * the same identity).
+ * Signals that the operation will result in duplicate Ingredients (Ingredients are considered duplicates
+ * if they have the same identity).
  */
 public class DuplicateIngredientException extends RuntimeException {
     public DuplicateIngredientException() {
