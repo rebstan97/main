94e1420b57c76b095dfcacf6ca6195fd24cb21e1
yican95 <yican95@gmail.com>
2018-10-09 23:32:21 +0800

    Refractor MenuParserUtilTest.java to ItemParserUtilTest.java

--- src/test/java/seedu/address/logic/parser/menu/ItemParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/menu/ItemParserUtilTest.java
@@ -2,26 +2,17 @@ package seedu.address.logic.parser.menu;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.logic.parser.ParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.menu.Name;
 import seedu.address.model.menu.Price;
-import seedu.address.model.tag.Tag;
 import seedu.address.testutil.Assert;
 
-public class MenuParserUtilTest {
+public class ItemParserUtilTest {
     private static final String INVALID_NAME = "F@ies";
     private static final String INVALID_PRICE = "+651234";
     private static final String INVALID_TAG = "#fries";
@@ -37,47 +28,47 @@ public class MenuParserUtilTest {
 
     @Test
     public void parseName_null_throwsNullPointerException() {
-        Assert.assertThrows(NullPointerException.class, () -> MenuParserUtil.parseName((String) null));
+        Assert.assertThrows(NullPointerException.class, () -> ItemParserUtil.parseName((String) null));
     }
 
     @Test
     public void parseName_invalidValue_throwsParseException() {
-        Assert.assertThrows(ParseException.class, () -> MenuParserUtil.parseName(INVALID_NAME));
+        Assert.assertThrows(ParseException.class, () -> ItemParserUtil.parseName(INVALID_NAME));
     }
 
     @Test
     public void parseName_validValueWithoutWhitespace_returnsName() throws Exception {
         Name expectedName = new Name(VALID_NAME);
-        assertEquals(expectedName, MenuParserUtil.parseName(VALID_NAME));
+        assertEquals(expectedName, ItemParserUtil.parseName(VALID_NAME));
     }
 
     @Test
     public void parseName_validValueWithWhitespace_returnsTrimmedName() throws Exception {
         String nameWithWhitespace = WHITESPACE + VALID_NAME + WHITESPACE;
         Name expectedName = new Name(VALID_NAME);
-        assertEquals(expectedName, MenuParserUtil.parseName(nameWithWhitespace));
+        assertEquals(expectedName, ItemParserUtil.parseName(nameWithWhitespace));
     }
 
     @Test
     public void parsePrice_null_throwsNullPointerException() {
-        Assert.assertThrows(NullPointerException.class, () -> MenuParserUtil.parsePrice((String) null));
+        Assert.assertThrows(NullPointerException.class, () -> ItemParserUtil.parsePrice((String) null));
     }
 
     @Test
     public void parsePrice_invalidValue_throwsParseException() {
-        Assert.assertThrows(ParseException.class, () -> MenuParserUtil.parsePrice(INVALID_PRICE));
+        Assert.assertThrows(ParseException.class, () -> ItemParserUtil.parsePrice(INVALID_PRICE));
     }
 
     @Test
     public void parsePrice_validValueWithoutWhitespace_returnsPhone() throws Exception {
         Price expectedPrice = new Price(VALID_PRICE);
-        assertEquals(expectedPrice, MenuParserUtil.parsePrice(VALID_PRICE));
+        assertEquals(expectedPrice, ItemParserUtil.parsePrice(VALID_PRICE));
     }
 
     @Test
     public void parsePrice_validValueWithWhitespace_returnsTrimmedPhone() throws Exception {
         String priceWithWhitespace = WHITESPACE + VALID_PRICE + WHITESPACE;
         Price expectedPrice = new Price(VALID_PRICE);
-        assertEquals(expectedPrice, MenuParserUtil.parsePrice(priceWithWhitespace));
+        assertEquals(expectedPrice, ItemParserUtil.parsePrice(priceWithWhitespace));
     }
 }
