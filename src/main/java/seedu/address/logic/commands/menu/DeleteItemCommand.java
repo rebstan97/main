d3f960b6a4220ea51ac79e89becf37c9dbf2b9ff
yican95 <yican95@gmail.com>
2018-10-10 18:42:20 +0800

    Logic: Add DeleteItemCommand.java

---
+++ src/main/java/seedu/address/logic/commands/menu/DeleteItemCommand.java
@@ -0,0 +1,59 @@
+package seedu.address.logic.commands.menu;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.menu.Item;
+
+/**
+ * Deletes a person identified using it's displayed index from the address book.
+ */
+public class DeleteItemCommand extends Command {
+
+    public static final String COMMAND_WORD = "delete-item";
+
+    public static final String COMMAND_ALIAS = "di";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the item identified by the index number used in the displayed item list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_ITEM_SUCCESS = "Deleted Item: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteItemCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        requireNonNull(model);
+        List<Item> lastShownList = model.getFilteredItemList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_ITEM_DISPLAYED_INDEX);
+        }
+
+        Item itemToDelete = lastShownList.get(targetIndex.getZeroBased());
+        model.deleteItem(itemToDelete);
+        model.commitAddressBook();
+        return new CommandResult(String.format(MESSAGE_DELETE_ITEM_SUCCESS, itemToDelete));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteItemCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteItemCommand) other).targetIndex)); // state check
+    }
+}
