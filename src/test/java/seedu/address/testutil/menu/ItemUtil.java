f0df698c4b3857cd08640f7f4df4dfe079156c60
yican95 <yican95@gmail.com>
2018-10-10 21:35:08 +0800

    ItemUtil.java: Add util for testing EditItemCommand

--- src/test/java/seedu/address/testutil/menu/ItemUtil.java
+++ src/test/java/seedu/address/testutil/menu/ItemUtil.java
@@ -4,8 +4,12 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.Set;
+
 import seedu.address.logic.commands.menu.AddItemCommand;
+import seedu.address.logic.commands.menu.EditItemCommand.EditItemDescriptor;
 import seedu.address.model.menu.Item;
+import seedu.address.model.tag.Tag;
 
 /**
  * A utility class for Item.
@@ -29,6 +33,24 @@ public class ItemUtil {
         item.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code EditItemDescriptor}'s details.
+     */
+    public static String getEditItemDescriptorDetails(EditItemDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
+        descriptor.getPrice().ifPresent(price -> sb.append(PREFIX_PRICE).append(price.value).append(" "));
+        if (descriptor.getTags().isPresent()) {
+            Set<Tag> tags = descriptor.getTags().get();
+            if (tags.isEmpty()) {
+                sb.append(PREFIX_TAG);
+            } else {
+                tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
+            }
+        }
         return sb.toString();
     }
 }
