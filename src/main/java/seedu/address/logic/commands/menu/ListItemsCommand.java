2dc07b0aa10213ed760dfc5547ee68a51d120eb3
yican95 <yican95@gmail.com>
2018-10-10 16:23:07 +0800

    Logic: Add ListItemsCommand.java

---
+++ src/main/java/seedu/address/logic/commands/menu/ListItemsCommand.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.commands.menu;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_ITEMS;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class ListItemsCommand extends Command {
+
+    public static final String COMMAND_WORD = "list-items";
+
+    public static final String COMMAND_ALIAS = "li";
+
+    public static final String MESSAGE_SUCCESS = "Listed all items";
+
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        model.updateFilteredItemList(PREDICATE_SHOW_ALL_ITEMS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
