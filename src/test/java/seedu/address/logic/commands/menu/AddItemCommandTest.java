28ec3d7889999fd92942f641569bac7303b0f342
yican95 <yican95@gmail.com>
2018-10-11 21:39:29 +0800

    AddItemCommandTest.java: Add SalesRecord method

--- src/test/java/seedu/address/logic/commands/menu/AddItemCommandTest.java
+++ src/test/java/seedu/address/logic/commands/menu/AddItemCommandTest.java
@@ -23,6 +23,7 @@ import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.accounts.Account;
 import seedu.address.model.menu.Item;
 import seedu.address.model.person.Person;
+import seedu.address.model.salesrecord.SalesRecord;
 import seedu.address.model.tag.Tag;
 import seedu.address.testutil.menu.ItemBuilder;
 
@@ -232,6 +233,36 @@ public class AddItemCommandTest {
 
         @Override
         public void updateFilteredItemList(Predicate<Item> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void addRecord(SalesRecord record) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public boolean hasRecord(SalesRecord record) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void deleteRecord(SalesRecord target) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateRecord(SalesRecord target, SalesRecord editedRecord) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<SalesRecord> getFilteredRecordList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateFilteredRecordList(Predicate<SalesRecord> predicate) {
             throw new AssertionError("This method should not be called.");
         }
     }
