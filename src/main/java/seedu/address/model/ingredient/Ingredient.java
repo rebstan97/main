c54d03fbe55cbe1996a6b1330c8f4bee9477b8a8
Rebecca Tan <rebecca_tan_97@hotmail.com>
2018-10-11 21:24:23 +0800

    Update constructor for Ingredient

--- src/main/java/seedu/address/model/ingredient/Ingredient.java
+++ src/main/java/seedu/address/model/ingredient/Ingredient.java
@@ -22,18 +22,6 @@ public class Ingredient {
     /**
      * Every field must be present and not null.
      */
-    public Ingredient(IngredientName name, IngredientUnit unit, IngredientPrice price, MinimumUnit minimum) {
-        requireAllNonNull(name);
-        this.name = name;
-        this.unit = unit;
-        this.price = price;
-        this.minimum = minimum;
-        this.numUnits = new NumUnits("0");
-    }
-
-    /**
-     * Every field must be present and not null.
-     */
     public Ingredient(IngredientName name, IngredientUnit unit, IngredientPrice price, MinimumUnit minimum,
                       NumUnits numUnits) {
         requireAllNonNull(name);
