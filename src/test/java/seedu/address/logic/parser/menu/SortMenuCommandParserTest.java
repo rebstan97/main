1a1e4f581465fd4124334b17945da3cf28c2feb1
yican95 <yican95@gmail.com>
2018-10-13 18:45:22 +0800

    Test: Add SortMenuCommandParserTest.java

---
+++ src/test/java/seedu/address/logic/parser/menu/SortMenuCommandParserTest.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.parser.menu;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.menu.SelectItemCommand;
+import seedu.address.logic.commands.menu.SortMenuCommand;
+import seedu.address.logic.commands.menu.SortMenuCommand.SortMethod;
+
+public class SortMenuCommandParserTest {
+
+    private SortMenuCommandParser parser = new SortMenuCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSortMenuCommand() {
+        // lower case
+        assertParseSuccess(parser, "name", new SortMenuCommand(SortMethod.NAME));
+        assertParseSuccess(parser, "price", new SortMenuCommand(SortMethod.PRICE));
+
+        // upper case
+        assertParseSuccess(parser, "NAME", new SortMenuCommand(SortMethod.NAME));
+        assertParseSuccess(parser, "PRICE", new SortMenuCommand(SortMethod.PRICE));
+
+        // camel case
+        assertParseSuccess(parser, "nAmE", new SortMenuCommand(SortMethod.NAME));
+        assertParseSuccess(parser, "pRiCE", new SortMenuCommand(SortMethod.PRICE));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "not name", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                SortMenuCommand.MESSAGE_USAGE));
+    }
+}
