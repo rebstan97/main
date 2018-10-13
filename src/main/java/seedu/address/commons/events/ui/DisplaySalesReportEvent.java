e861a39d1c5e9ee55a12a24e55e444bee7d5933a
HyperionNKJ <kjneo1996@gmail.com>
2018-10-13 18:25:20 +0800

    DisplaySalesReportEvent.java: Add date field, constructor and getter

--- src/main/java/seedu/address/commons/events/ui/DisplaySalesReportEvent.java
+++ src/main/java/seedu/address/commons/events/ui/DisplaySalesReportEvent.java
@@ -1,11 +1,22 @@
 package seedu.address.commons.events.ui;
 
 import seedu.address.commons.events.BaseEvent;
+import seedu.address.model.salesrecord.Date;
 
 /**
- * An event requesting to display the sales report.
+ * An event requesting to display the sales report of a specified date
  */
 public class DisplaySalesReportEvent extends BaseEvent {
+
+    private Date dateOfSalesReportToDisplay;
+
+    public DisplaySalesReportEvent(Date dateOfSalesReportToDisplay) {
+        this.dateOfSalesReportToDisplay = dateOfSalesReportToDisplay;
+    }
+
+    public Date getDateOfSalesReportToDisplay() {
+        return dateOfSalesReportToDisplay;
+    }
 
     @Override
     public String toString() {
