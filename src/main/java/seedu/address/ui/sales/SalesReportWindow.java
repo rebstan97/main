f68e6045c07e1fd9ec5393dcbc167560b2b05669
HyperionNKJ <kjneo1996@gmail.com>
2018-10-14 16:59:10 +0800

    SalesReportWindow: Display sales report in window

--- src/main/java/seedu/address/ui/sales/SalesReportWindow.java
+++ src/main/java/seedu/address/ui/sales/SalesReportWindow.java
@@ -1,14 +1,19 @@
 package seedu.address.ui.sales;
 
+import java.text.NumberFormat;
 import java.util.logging.Logger;
 
-
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.salesrecord.ItemName;
+import seedu.address.model.salesrecord.Price;
+import seedu.address.model.salesrecord.QuantitySold;
 import seedu.address.model.salesrecord.SalesRecord;
 import seedu.address.model.salesrecord.SalesReport;
 import seedu.address.ui.UiPart;
@@ -20,15 +25,24 @@ public class SalesReportWindow extends UiPart<Stage> {
 
     private static final Logger logger = LogsCenter.getLogger(SalesReportWindow.class);
     private static final String FXML = "SalesReportWindow.fxml";
+    private static final String title = "Sales Report for %s";
+    private static final String totalRevenueMessage = "Total revenue: %s";
+    private static final NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
 
     private SalesReport salesReport;
 
     @FXML
-    private TableView<SalesRecord> tableView;
-//    @FXML
-//    private Tabl
+    private TableView<SalesRecord> recordsTable;
     @FXML
-    private Label date;
+    private TableColumn<SalesRecord, ItemName> itemName;
+    @FXML
+    private TableColumn<SalesRecord, QuantitySold> quantitySold;
+    @FXML
+    private TableColumn<SalesRecord, Price> price;
+    @FXML
+    private TableColumn<SalesRecord, Double> revenue;
+    @FXML
+    private Label totalRevenue;
 
     /**
      * Creates a new SalesReportWindow
@@ -38,6 +52,7 @@ public class SalesReportWindow extends UiPart<Stage> {
      */
     public SalesReportWindow(Stage root, SalesReport salesReport) {
         super(FXML, root);
+        root.setTitle(String.format(title, salesReport.getDate().toString()));
         this.salesReport = salesReport;
     }
 
@@ -68,7 +83,40 @@ public class SalesReportWindow extends UiPart<Stage> {
      */
     public void show() {
         logger.fine("Displaying sales report.");
+        initialize();
         getRoot().show();
+    }
+
+    private void initialize(){
+        itemName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        quantitySold.setCellValueFactory(new PropertyValueFactory<>("quantitySold"));
+        initializePriceColumn();
+        initializeRevenueColumn();
+        recordsTable.setItems(salesReport.getRecords());
+        totalRevenue.setText(String.format(totalRevenueMessage,
+                currencyFormatter.format(salesReport.getTotalRevenue())));
+    }
+
+    private void initializePriceColumn() {
+        price.setCellValueFactory(new PropertyValueFactory<>("price"));
+        price.setCellFactory(cell -> new TableCell<>() {
+            @Override
+            protected void updateItem(Price price, boolean empty) {
+                super.updateItem(price, empty);
+                setText((empty) ? null : currencyFormatter.format(price.getValue()));
+            }
+        });
+    }
+
+    private void initializeRevenueColumn() {
+        revenue.setCellValueFactory(new PropertyValueFactory<>("revenue"));
+        revenue.setCellFactory(cell -> new TableCell<>() {
+            @Override
+            protected void updateItem(Double revenue, boolean empty) {
+                super.updateItem(revenue, empty);
+                setText((empty) ? null : currencyFormatter.format(revenue));
+            }
+        });
     }
 
     /**
