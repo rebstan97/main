bd3307a922a7aa81cc20b2bd19d89b7597112d7e
HyperionNKJ <kjneo1996@gmail.com>
2018-10-16 02:10:09 +0800

    RecordUtil: Add method to obtain user input given descriptor

--- src/test/java/seedu/address/testutil/salesrecords/RecordUtil.java
+++ src/test/java/seedu/address/testutil/salesrecords/RecordUtil.java
@@ -4,6 +4,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ITEM_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ITEM_PRICE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_QUANTITY_SOLD;
 
+import seedu.address.logic.commands.salescommands.EditSalesCommand.EditRecordDescriptor;
 import seedu.address.logic.commands.salescommands.RecordSalesCommand;
 import seedu.address.model.salesrecord.SalesRecord;
 /**
@@ -25,6 +26,17 @@ public class RecordUtil {
                 + PREFIX_QUANTITY_SOLD + record.getQuantitySold().toString() + " "
                 + PREFIX_ITEM_PRICE + record.getPrice().toString();
     }
+
+    /**
+     * Returns the part of command string for the given {@code EditRecordDescriptor}'s details.
+     */
+    public static String getEditRecordDescriptorDetails(EditRecordDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        descriptor.getDate().ifPresent(date -> sb.append(PREFIX_DATE).append(date.toString()).append(" "));
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_ITEM_NAME).append(name.toString()).append(" "));
+        descriptor.getQuantitySold().ifPresent(
+                quantitySold-> sb.append(PREFIX_QUANTITY_SOLD).append(quantitySold.toString()).append(" "));
+        descriptor.getPrice().ifPresent(price -> sb.append(PREFIX_ITEM_PRICE).append(price.toString()).append(" "));
+        return sb.toString();
+    }
 }
-
-
