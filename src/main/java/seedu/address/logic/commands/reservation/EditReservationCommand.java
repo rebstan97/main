d4fa6aab492f590c8d37933adfa4cca65f8e4c8a
m4dkip <ongmingxian@gmail.com>
2018-10-16 22:29:04 +0800

    EditReservationCommand.java: Update message

--- src/main/java/seedu/address/logic/commands/reservation/EditReservationCommand.java
+++ src/main/java/seedu/address/logic/commands/reservation/EditReservationCommand.java
@@ -50,7 +50,7 @@ public class EditReservationCommand extends Command {
 
     public static final String MESSAGE_EDIT_RESERVATION_SUCCESS = "Edited Reservation: %1$s";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    public static final String MESSAGE_DUPLICATE_RESERVATION = "This reservation already exists in the menu.";
+    public static final String MESSAGE_DUPLICATE_RESERVATION = "This reservation already exists.";
 
     private final Index index;
     private final EditReservationDescriptor editReservationDescriptor;
