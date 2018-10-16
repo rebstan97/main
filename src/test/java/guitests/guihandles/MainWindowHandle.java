a9750040c2a06b3e66448d71f072a127f244651f
AZhiKai <ang-zhikai@outlook.com>
2018-10-17 01:08:16 +0800

    MainWindowHandle: Remove unused method

--- src/test/java/guitests/guihandles/MainWindowHandle.java
+++ src/test/java/guitests/guihandles/MainWindowHandle.java
@@ -13,7 +13,6 @@ public class MainWindowHandle extends StageHandle {
     private final StatusBarFooterHandle statusBarFooter;
     private final MainMenuHandle mainMenu;
     private final BrowserPanelHandle browserPanel;
-    private final UsernameHandle username;
 
     public MainWindowHandle(Stage stage) {
         super(stage);
@@ -24,7 +23,6 @@ public class MainWindowHandle extends StageHandle {
         statusBarFooter = new StatusBarFooterHandle(getChildNode(StatusBarFooterHandle.STATUS_BAR_PLACEHOLDER));
         mainMenu = new MainMenuHandle(getChildNode(MainMenuHandle.MENU_BAR_ID));
         browserPanel = new BrowserPanelHandle(getChildNode(BrowserPanelHandle.BROWSER_ID));
-        username = new UsernameHandle(getChildNode(UsernameHandle.USERNAME_ID));
     }
 
     public PersonListPanelHandle getPersonListPanel() {
@@ -49,9 +47,5 @@ public class MainWindowHandle extends StageHandle {
 
     public BrowserPanelHandle getBrowserPanel() {
         return browserPanel;
-    }
-
-    public UsernameHandle getUsername() {
-        return username;
     }
 }
