e1eee1e1d67a0ea015e0d7195b6a3a288a06daa1
m4dkip <ongmingxian@gmail.com>
2018-10-16 22:42:32 +0800

    logic/parser: Integrate EditReservationCommand to AddressBookParser.java

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -32,6 +32,7 @@ import seedu.address.logic.commands.menu.EditItemCommand;
 import seedu.address.logic.commands.menu.ListItemsCommand;
 import seedu.address.logic.commands.menu.SelectItemCommand;
 import seedu.address.logic.commands.reservation.AddReservationCommand;
+import seedu.address.logic.commands.reservation.EditReservationCommand;
 import seedu.address.logic.commands.sales.DisplaySalesCommand;
 import seedu.address.logic.commands.sales.RecordSalesCommand;
 import seedu.address.logic.parser.accounts.RegisterCommandParser;
@@ -44,6 +45,7 @@ import seedu.address.logic.parser.menu.DeleteItemCommandParser;
 import seedu.address.logic.parser.menu.EditItemCommandParser;
 import seedu.address.logic.parser.menu.SelectItemCommandParser;
 import seedu.address.logic.parser.reservation.AddReservationCommandParser;
+import seedu.address.logic.parser.reservation.EditReservationCommandParser;
 import seedu.address.logic.parser.sales.DisplaySalesCommandParser;
 import seedu.address.logic.parser.sales.RecordSalesCommandParser;
 
@@ -176,6 +178,10 @@ public class AddressBookParser {
         case AddReservationCommand.COMMAND_WORD:
         case AddReservationCommand.COMMAND_ALIAS:
             return new AddReservationCommandParser().parse(arguments);
+
+        case EditReservationCommand.COMMAND_WORD:
+        case EditReservationCommand.COMMAND_ALIAS:
+            return new EditReservationCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
