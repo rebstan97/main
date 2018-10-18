37ed644fa1e35635cab4dd66addf9943760923a8
AZhiKai <ang-zhikai@outlook.com>
2018-10-19 01:07:09 +0800

    Account: Add tests for new constructor

--- src/main/java/seedu/address/model/accounts/Account.java
+++ src/main/java/seedu/address/model/accounts/Account.java
@@ -71,6 +71,9 @@ public class Account {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
+        if (password == null) {
+            return Objects.hash(username);
+        }
         return Objects.hash(username, password);
     }
 
