c4d4a32f4d9e1fb8f04d1ff917a3927c8df08f15
AZhiKai <ang-zhikai@outlook.com>
2018-10-27 16:04:01 +0800

    ChangePasswordCommandParserTest: Update tests
    Cleaned up tests with constant variables.

--- src/test/java/seedu/address/logic/parser/accounts/ChangePasswordCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/accounts/ChangePasswordCommandParserTest.java
@@ -1,7 +1,8 @@
 package seedu.address.logic.parser.accounts;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.PREFIX_WITH_INVALID_PASSWORD;
+import static seedu.address.logic.commands.CommandTestUtil.PREFIX_WITH_INVALID_NEW_PASSWORD;
+import static seedu.address.logic.commands.CommandTestUtil.PREFIX_WITH_VALID_NEW_PASSWORD;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PASSWORD_DEMO_ONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NEW_PASSWORD;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -16,8 +17,8 @@ import seedu.address.testutil.accounts.EditAccountDescriptorBuilder;
 
 public class ChangePasswordCommandParserTest {
 
-    private static final String MESSAGE_INVALID_FORMAT =
-            String.format(MESSAGE_INVALID_COMMAND_FORMAT, ChangePasswordCommand.MESSAGE_USAGE);
+    private static final String MESSAGE_INVALID_FORMAT = String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            ChangePasswordCommand.MESSAGE_USAGE);
 
     private ChangePasswordCommandParser parser = new ChangePasswordCommandParser();
 
@@ -32,19 +33,19 @@ public class ChangePasswordCommandParserTest {
 
     @Test
     public void parse_invalidPreamble_failure() {
-        // negative index
-        assertParseFailure(parser, PREFIX_WITH_INVALID_PASSWORD, MESSAGE_INVALID_FORMAT);
+        // invalid password
+        assertParseFailure(parser, "random " + PREFIX_WITH_VALID_NEW_PASSWORD, MESSAGE_INVALID_FORMAT);
 
         // invalid arguments being parsed as preamble
         assertParseFailure(parser, "1 some random string", MESSAGE_INVALID_FORMAT);
 
         // invalid prefix being parsed as preamble
-        assertParseFailure(parser, "1 i/ string", MESSAGE_INVALID_FORMAT);
+        assertParseFailure(parser, "1 " + PREFIX_WITH_VALID_NEW_PASSWORD, MESSAGE_INVALID_FORMAT);
     }
 
     @Test
     public void parse_invalidValue_failure() {
-        assertParseFailure(parser, " " + PREFIX_NEW_PASSWORD + "1122 qq",
+        assertParseFailure(parser, " " + PREFIX_WITH_INVALID_NEW_PASSWORD,
                 Password.MESSAGE_PASSWORD_CONSTRAINT); // invalid password
     }
 
