df9c33d20500eac78b510c8989eecf94831086f0
yican95 <yican95@gmail.com>
2018-10-29 18:28:09 +0800

    Logic: Add RequiredIngredientsItemCommandParser.java

---
+++ src/main/java/seedu/address/logic/parser/menu/RequiredIngredientsItemCommandParser.java
@@ -0,0 +1,88 @@
+package seedu.address.logic.parser.menu;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INGREDIENT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INGREDIENT_NUM;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.menu.RequiredIngredientsItemCommand;
+import seedu.address.logic.parser.ArgumentIndexAndPairMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.ingredient.IngredientName;
+
+/**
+ * Parses input arguments and creates a new RequiredIngredientsItemCommand object
+ */
+public class RequiredIngredientsItemCommandParser implements Parser<RequiredIngredientsItemCommand> {
+    public static final String MESSAGE_INTEGER_CONSTRAINTS =
+            "Number of ingredients should only contain positive numbers";
+    public static final String INTEGER_VALIDATION_REGEX = "\\d+";
+    public static final String MESSAGE_DUPLICATE_INGREDIENT_NAME = "Duplicated ingredients entered";
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the RecipeItemCommand
+     * and returns an RequiredIngredientsItemCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public RequiredIngredientsItemCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        ArgumentIndexAndPairMultimap argMultimap =
+                ArgumentTokenizer.tokenizeToIndexAndPair(args, PREFIX_INGREDIENT_NAME, PREFIX_INGREDIENT_NUM);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getValue(0).get(new Prefix("")));
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    RequiredIngredientsItemCommand.MESSAGE_USAGE), pe);
+        }
+
+        int count = 1;
+        Map<IngredientName, Integer> requiredIngredients = new HashMap<>();
+        while (argMultimap.contains(count)) {
+            Map<Prefix, String> map = argMultimap.getValue(count);
+
+            if (!map.containsKey(PREFIX_INGREDIENT_NAME)) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        RequiredIngredientsItemCommand.MESSAGE_USAGE));
+            }
+            IngredientName name = ParserUtil.parseIngredientName(map.get(PREFIX_INGREDIENT_NAME));
+            if (requiredIngredients.containsKey(name)) {
+                throw new ParseException(MESSAGE_DUPLICATE_INGREDIENT_NAME);
+            }
+
+            if (!map.containsKey(PREFIX_INGREDIENT_NUM)) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                        RequiredIngredientsItemCommand.MESSAGE_USAGE));
+            }
+            String trimmedInteger = map.get(PREFIX_INGREDIENT_NUM);
+            if (!isValidInteger(trimmedInteger)) {
+                throw new ParseException(MESSAGE_INTEGER_CONSTRAINTS);
+            }
+            Integer num = Integer.parseInt(trimmedInteger);
+
+            requiredIngredients.put(name, num);
+            count++;
+        }
+
+        return new RequiredIngredientsItemCommand(index, requiredIngredients);
+    }
+
+    public static boolean isValidInteger(String test) {
+        if (!test.matches(INTEGER_VALIDATION_REGEX)) {
+            return false;
+        }
+        return Integer.parseInt(test) > 0;
+    }
+
+}
