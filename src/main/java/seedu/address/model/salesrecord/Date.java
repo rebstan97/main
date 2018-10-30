41a4c6c2031f4c0f86028388d521596cd4ab8945
HyperionNKJ <kjneo1996@gmail.com>
2018-10-31 02:32:55 +0800

    Date: Add dayOfWeek field

--- src/main/java/seedu/address/model/salesrecord/Date.java
+++ src/main/java/seedu/address/model/salesrecord/Date.java
@@ -3,10 +3,13 @@ package seedu.address.model.salesrecord;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.ResolverStyle;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 /**
  * Represents a date in the calendar in DD-MM-YYYY format Guarantees: immutable; is valid as declared in {@link
@@ -17,8 +20,10 @@ public class Date {
     public static final String MESSAGE_DATE_CONSTRAINTS =
             "Dates should only contain numbers, and it should be in the format DD-MM-YYYY.\nThe date must exist in "
                     + "the calendar";
+    public static final String DATE_FORMAT_PATTERN = "dd-MM-uuuu";
 
     private final String date;
+    private final String dayOfWeek;
 
     /**
      * Constructs a {@code date}.
@@ -29,6 +34,9 @@ public class Date {
         requireNonNull(date);
         checkArgument(isValidDate(date), MESSAGE_DATE_CONSTRAINTS);
         this.date = date;
+        DateTimeFormatter validFormat =
+                DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN).withResolverStyle(ResolverStyle.STRICT);
+        dayOfWeek = LocalDate.parse(date, validFormat).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.US);
     }
 
     /**
@@ -36,13 +44,17 @@ public class Date {
      */
     public static boolean isValidDate(String test) {
         DateTimeFormatter validFormat =
-                DateTimeFormatter.ofPattern("dd-MM-uuuu").withResolverStyle(ResolverStyle.STRICT);
+                DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN).withResolverStyle(ResolverStyle.STRICT);
         try {
             LocalDate.parse(test, validFormat);
         } catch (DateTimeParseException e) {
             return false;
         }
         return true;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
     }
 
     @Override
