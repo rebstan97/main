bff289dbf26c371faf2491971e99042c0723ec86
AZhiKai <ang-zhikai@outlook.com>
2018-10-31 22:49:43 +0800

    AccountListPanel: Update comments

--- src/main/java/seedu/address/ui/accounts/AccountListPanel.java
+++ src/main/java/seedu/address/ui/accounts/AccountListPanel.java
@@ -2,22 +2,18 @@ package seedu.address.ui.accounts;
 
 import java.util.logging.Logger;
 
-import com.google.common.eventbus.Subscribe;
-
-import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
 import seedu.address.commons.events.ui.accounts.AccountPanelSelectionChangedEvent;
 import seedu.address.model.accounts.Account;
 import seedu.address.ui.UiPart;
 
 /**
- * Panel containing the list of items.
+ * Panel containing the list of accounts.
  */
 public class AccountListPanel extends UiPart<Region> {
 
@@ -40,33 +36,16 @@ public class AccountListPanel extends UiPart<Region> {
     }
 
     private void setEventHandlerForSelectionChangeEvent() {
-        accountListView.getSelectionModel().selectedItemProperty()
-                .addListener((observable, oldValue, newValue) -> {
-                    if (newValue != null) {
-                        logger.fine("Selection in item list panel changed to : '" + newValue + "'");
-                        raise(new AccountPanelSelectionChangedEvent(newValue));
-                    }
-                });
-    }
-
-    /**
-     * Scrolls to the {@code ItemCard} at the {@code index} and selects it.
-     */
-    private void scrollTo(int index) {
-        Platform.runLater(() -> {
-            accountListView.scrollTo(index);
-            accountListView.getSelectionModel().clearAndSelect(index);
+        accountListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                logger.fine("Selection in item list panel changed to : '" + newValue + "'");
+                raise(new AccountPanelSelectionChangedEvent(newValue));
+            }
         });
     }
 
-    @Subscribe
-    private void handleJumpToListRequestEvent(JumpToListRequestEvent event) {
-        logger.info(LogsCenter.getEventHandlingLogMessage(event));
-        scrollTo(event.targetIndex);
-    }
-
     /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Item} using a {@code ItemCard}.
+     * Custom {@code ListCell} that displays the graphics of a {@code Account} using a {@code AccountCard}.
      */
     class AccountListViewCell extends ListCell<Account> {
 
