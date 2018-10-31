08f1cd7082bb2a0d6b5fca6e5fae082dbd6b6143
AZhiKai <ang-zhikai@outlook.com>
2018-10-31 23:55:34 +0800

    Remove account panel selection
    No intention to display account on the browser panel, thus the panel selection event is not required.

--- src/main/java/seedu/address/ui/accounts/AccountListPanel.java
+++ src/main/java/seedu/address/ui/accounts/AccountListPanel.java
@@ -1,14 +1,10 @@
 package seedu.address.ui.accounts;
 
-import java.util.logging.Logger;
-
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.commons.events.ui.accounts.AccountPanelSelectionChangedEvent;
 import seedu.address.model.accounts.Account;
 import seedu.address.ui.UiPart;
 
@@ -18,7 +14,6 @@ import seedu.address.ui.UiPart;
 public class AccountListPanel extends UiPart<Region> {
 
     private static final String FXML = "AccountListPanel.fxml";
-    private final Logger logger = LogsCenter.getLogger(AccountListPanel.class);
 
     @FXML
     private ListView<Account> accountListView;
@@ -32,16 +27,6 @@ public class AccountListPanel extends UiPart<Region> {
     private void setConnections(ObservableList<Account> itemList) {
         accountListView.setItems(itemList);
         accountListView.setCellFactory(listView -> new AccountListViewCell());
-        setEventHandlerForSelectionChangeEvent();
-    }
-
-    private void setEventHandlerForSelectionChangeEvent() {
-        accountListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                logger.fine("Selection in item list panel changed to : '" + newValue + "'");
-                raise(new AccountPanelSelectionChangedEvent(newValue));
-            }
-        });
     }
 
     /**
